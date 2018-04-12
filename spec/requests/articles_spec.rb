50825201fa9c94f0e561d9ab5caf75c19b5fd00f
Leslie Kornes <lesliekornes@gmail.com>
2018-04-12 00:39:50 -0400

    Create spec/requests and add articles_spec to that dir.

---
+++ spec/requests/articles_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "Articles", type: :request do
+
+  before do
+    @article = Article.create(title: "Article Title", body: "Body body of the article!")
+  end
+
+  describe 'GET /articles/:id' do
+    context 'with existing article' do
+      before { get "/articles/#{@article.id}" }
+
+      it "handles existing article" do
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'with non-existant article' do
+      before { get "/articles/xxxx" }
+
+      it "handles non-existant article" do
+        expect(response.status).to eq 404
+        flash_message = "The article you requested can't be found"
+        expect(flash[:alert]).to eq flash_message
+      end
+    end
+  end
+
+end
