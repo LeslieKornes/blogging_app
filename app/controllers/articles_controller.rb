bdf07eb21721f7b9495c47c02b5e4fb32b5ed957
Leslie Kornes <lesliekornes@gmail.com>
2018-04-15 00:16:05 -0400

    Extract set_article method in ArticlesController.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def destroy
-    @article = Article.find(params[:id])
+    set_article
     if @article.destroy
       flash[:success] = "Article has been deleted!"
       redirect_to articles_path
@@ -28,15 +29,15 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+    set_article
   end
 
   def show
-    @article = Article.find(params[:id])
+    set_article
   end
 
   def update
-    @article = Article.find(params[:id])
+    set_article
     if @article.update(article_params)
       flash[:success] = "Article has been updated!"
       redirect_to @article
@@ -54,6 +55,10 @@ class ArticlesController < ApplicationController
     end
 
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
     def article_params
       params.require(:article).permit(:title, :body)
     end
